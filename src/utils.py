305fc53b7a54f1d28b91a4e284223bddd0dffbd9
drsnehalyerne-ai <dr.snehalyerne@gmail.com>
2026-06-14 22:53:35 +0530

    Utils.py

--- src/utils.py
+++ src/utils.py
@@ -7,9 +7,6 @@ import gzip
 
 
 def read_geo_series_matrix(filepath):
-    """
-    Read GEO Series Matrix file.
-    """
 
     with gzip.open(filepath, "rt", errors="ignore") as f:
         lines = f.readlines()
@@ -17,27 +14,30 @@ def read_geo_series_matrix(filepath):
     start = None
 
     for i, line in enumerate(lines):
+
         if line.startswith('"ID_REF"'):
             start = i
             break
 
     if start is None:
-        from io import StringIO
+        raise ValueError(
+            "Expression matrix not found."
+        )
 
-end = None
+    end = None
 
-for i, line in enumerate(lines):
+    for i, line in enumerate(lines):
 
-    if line.startswith("!series_matrix_table_end"):
-        end = i
-        break
+        if line.startswith("!series_matrix_table_end"):
+            end = i
+            break
 
-if end is None:
-    end = len(lines)
+    if end is None:
+        end = len(lines)
 
-expr = pd.read_csv(
-    StringIO("".join(lines[start:end])),
-    sep="\t"
-)
+    expr = pd.read_csv(
+        StringIO("".join(lines[start:end])),
+        sep="\t"
+    )
 
-return expr
+    return expr
