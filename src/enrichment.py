9b7d962b27b2d2d5271ae0392545dc112beaf2bb
drsnehalyerne-ai <dr.snehalyerne@gmail.com>
2026-06-20 18:49:43 +0530

    enrichment.py

---
+++ src/enrichment.py
@@ -0,0 +1,16 @@
+"""
+Functional Enrichment Module
+"""
+
+def run_go_enrichment(gene_list):
+    """
+    Placeholder for GO enrichment
+    """
+    return None
+
+
+def run_kegg_enrichment(gene_list):
+    """
+    Placeholder for KEGG enrichment
+    """
+    return None
