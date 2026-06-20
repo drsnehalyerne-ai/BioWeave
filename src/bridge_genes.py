9d2aabdb910700c15929a02cec6eccd82a668f51
drsnehalyerne-ai <dr.snehalyerne@gmail.com>
2026-06-20 17:45:40 +0530

    bridge_genes.py

---
+++ src/bridge_genes.py
@@ -0,0 +1,20 @@
+def find_bridge_genes(
+    dysplasia_deg_annot,
+    cancer_deg_annot
+):
+
+    dysplasia_genes = set(
+        dysplasia_deg_annot["Gene symbol"].dropna()
+    )
+
+    cancer_genes = set(
+        cancer_deg_annot["Gene symbol"].dropna()
+    )
+
+    return sorted(
+        list(
+            dysplasia_genes.intersection(
+                cancer_genes
+            )
+        )
+    )
